89a84f6890d6a2b98506e9758f976ac533e8bae8
caparicio-esd <caparicio@esdmadrid.es>
2023-01-17 21:14:17 +0100

    head config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/main.sass'
-import type { AppProps } from 'next/app'
+import "../styles/main.sass";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>hola</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
