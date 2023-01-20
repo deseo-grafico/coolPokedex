07bb48c194f8e9779e43b1704b21a068622e6ef0
AndreaCascallana <acascallana3016@esdmadrid.es>
2023-01-20 13:44:32 +0100

    (Dev) Add navigation to PokeSingle in PokeMainList

--- components/pokemon/PokemonMainList/PokemonMainList.tsx
+++ components/pokemon/PokemonMainList/PokemonMainList.tsx
@@ -1,19 +1,28 @@
 import { Box, Grid } from "@mantine/core";
+import Link from "next/link";
 import React from "react";
-type pokemonGridItem={
-  name: string;
-  picture: string
-};
 
+type pokemonGridItem = {
+  name: string;
+  picture: string;
+};
 
 const PokemonMainList = ({ pokemon }: any) => {
   return (
     <Grid className="hola" gutter={5} gutterXs="md" gutterMd="xl" gutterXl={50}>
-      {pokemon.pokemon.map((pokemonItem: any, i: number) => (
-        <Grid.Col span={4} md={6} lg={3} key={i} >
-          <Box bg="indigo">
-            <h4>{pokemonItem.name}</h4>
-            <img src={pokemonItem.picture} />
+      {pokemon.pokemon.map((pokemonItem: any, i: number, pokemonName: any) => (
+        <Grid.Col span={4} md={6} lg={3} key={i}>
+          <Box>
+            <Link
+              href={`pokemon/` + pokemonItem.name}
+            >
+              {pokemonItem.name}
+            </Link>
+            <Link
+              href={`pokemon/` + pokemonItem.name}
+            >
+              <img src={pokemonItem.picture} />
+            </Link>
           </Box>
         </Grid.Col>
       ))}
