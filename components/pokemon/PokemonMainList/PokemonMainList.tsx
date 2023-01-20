7ce5544dcae6abf4b044dd1408f56c9f07dfe397
AndreaCascallana <acascallana3016@esdmadrid.es>
2023-01-20 15:27:52 +0100

    (Pretty) Fix Mantine properties

--- components/pokemon/PokemonMainList/PokemonMainList.tsx
+++ components/pokemon/PokemonMainList/PokemonMainList.tsx
@@ -9,19 +9,25 @@ type pokemonGridItem = {
 
 const PokemonMainList = ({ pokemon }: any) => {
   return (
-    <Grid className="hola" gutter={5} gutterXs="md" gutterMd="xl" gutterXl={50}>
+    <Grid
+      className="hola"
+      gutter={5}
+      gutterXs="md"
+      gutterMd="xl"
+      gutterXl={50}
+      justify="center"
+    >
       {pokemon.pokemon.map((pokemonItem: any, i: number, pokemonName: any) => (
         <Grid.Col span={4} md={6} lg={3} key={i}>
-          <Box>
-            <Link
-              href={`pokemon/` + pokemonItem.name}
-            >
-              {pokemonItem.name}
-            </Link>
-            <Link
-              href={`pokemon/` + pokemonItem.name}
-            >
-              <img src={pokemonItem.picture} />
+          <Box
+            sx={(theme) => ({
+              textAlign: "center",
+              padding: theme.spacing.xs,
+            })}
+          >
+            <Link href={`pokemon/` + pokemonItem.name}>{pokemonItem.name}</Link>
+            <Link href={`pokemon/` + pokemonItem.name}>
+              <img src={pokemonItem.picture} className="mx-auto"/>
             </Link>
           </Box>
         </Grid.Col>
