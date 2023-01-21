c26f304cf1ee45832060401423d53800ae85047e
AndreaCascallana <acascallana3016@esdmadrid.es>
2023-01-21 14:00:22 +0100

    (Clean) Clean index's imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "@next/font/google";
-import Link from "next/link";
 import Button from "./../components/Button/Button";
 import PokemonMainList from "../components/pokemon/PokemonMainList/PokemonMainList";
 import { GetServerSidePropsContext } from "next/types";
