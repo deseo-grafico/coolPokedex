670d42b04f976dbcb217ff0fca3bf6276037fd0c
deseo-grafico <deseografico.natalia@gmail.com>
2023-01-22 19:34:03 +0100

    Fix: filter sccreen and components

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,30 +1,45 @@
+import { Box } from "@mantine/core";
+import { useState, MouseEventHandler } from "react";
 import classNames from "classnames";
-import { MouseEventHandler } from "react";
+
 import { stringToIcon } from '../../utils/stringToIcon';
 import styles from "./Filter.module.sass";
 
-type PokemonType = "normal" | "fire"| "water"| "grass"| "electric" | "ice" | "fighting" | "poison" | "ground" | "flying" | "psychic"| "bug" | "rock" | "ghost"| "dark" | "dragon"| "steel"| "fairy"
+// type PokemonType = "normal" | "fire"| "water"| "grass"| "electric" | "ice" | "fighting" | "poison" | "ground" | "flying" | "psychic"| "bug" | "rock" | "ghost"| "dark" | "dragon"| "steel"| "fairy"
 
 type FilterProps = {
   icon: string, 
-  filterType?: PokemonType,
-  filterActive: boolean, 
+  filterType?: string,
+  filterLabel: string,
+  // filterActive: boolean, 
   clickHandler?: MouseEventHandler<HTMLElement>,
 }
 
-const Filter = ({ clickHandler, filterType, filterActive, icon }: FilterProps) => {
+const Filter = ({ filterType, filterLabel, icon, clickHandler }: FilterProps) => {
+  let [isFilterSingleActive, setIsFilterSingleActive]=useState(false);
+
+  const toggleActive = () => {
+    setIsFilterSingleActive(!isFilterSingleActive);
+    console.log("filtro activado");
+    clickHandler
+    
+  };
   
   const FilterClasses = classNames(styles.filter, {
-    [`${styles.active}`]: filterActive == true,
-    [`${styles.inactive}`]: filterActive == false,
-    //[`${styles.filterType}`]: filterType == "type",
+    [`${styles.active}`]: isFilterSingleActive == true,
+    [`${styles.inactive}`]: isFilterSingleActive == false,
+    // [`${styles.filterType}`]: filterType == "type",
   });
   const Icon= stringToIcon(icon)
   return (
-    <>
-      <div className={FilterClasses} onClick={clickHandler}>
+    <><Box className="justify-center flex flex-col items-center">
+      <div className={FilterClasses} onClick={toggleActive}>
         {icon && <span className="icon">{<Icon height={18} />}</span>}
       </div>
+      <div className="filterLabel text-center text-sm">{filterLabel}</div>
+      
+    </Box>
+      
     </>
   );
 };
--- components/FilterScreen/FilterScreen.tsx
+++ components/FilterScreen/FilterScreen.tsx
@@ -1,11 +1,67 @@
 import { RangeSlider } from "@mantine/core";
-import React, { useContext } from "react";
+import { Box, Grid } from "@mantine/core";
+import React, { useContext, useState } from "react";
 import { IndexContext } from "../../contexts/indexContext";
 import Button from "../Button/Button";
 import Filter from "../Filter/Filter";
+// const PokemonTypes:Array<string>=[
+//   "normal",
+//   "fire",
+//   "water",
+//   "grass",
+//   "electric", "ice",
+//   "fighting",
+//   "poison",
+//   "ground",
+//   "flying",
+//   "psychic",
+//   "bug",
+//   "rock",
+//   "ghost",
+//   "dark",
+//   "dragon",
+//   "steel",
+//   "fairy"
+
+// ]
 
 const FilterScreen = ({ props }: any) => {
   let { toggleFilter } = useContext(IndexContext)!;
+
+  const [endValue, setEndValue] = useState([0, 100 / 8]);
+
+  const PokemonTypes: Array<string> = [
+    "normal",
+    "fire",
+    "water",
+    "grass",
+    "electric",
+    "ice",
+    "fighting",
+    "poison",
+    "ground",
+    "flying",
+    "psychic",
+    "bug",
+    "rock",
+    "ghost",
+    "dark",
+    "dragon",
+    "steel",
+    "fairy",
+  ];
+
+  const MARKS: { value: number; label: string }[] = [
+    
+    { value: 100 / 8, label: "1" },
+    { value: 200 / 8, label: "2" },
+    { value: 300 / 8, label: "3" },
+    { value: 50, label: "4" },
+    { value: 500 / 8, label: "5" },
+    { value: 600 / 8, label: "6" },
+    { value: 700 / 8, label: "7" },
+    { value: 100, label: "8" },
+  ];
 
   return (
     <>
@@ -15,36 +71,43 @@ const FilterScreen = ({ props }: any) => {
         clickHandler={toggleFilter}
       ></Button>
       <h1>FilterScreen</h1>
-      <div className="filtroSlider">
+      <div className="filtroSlider py-4">
         <h2>Generación</h2>
         <RangeSlider
           color="grape"
           size="lg"
           radius="xl"
-          // labelAlwaysOn
-          marks={[
-            { value: 100 / 8, label: "1" },
-            { value: 200 / 8, label: "2" },
-            { value: 300 / 8, label: "3" },
-            { value: 50, label: "4" },
-            { value: 500 / 8, label: "5" },
-            { value: 600 / 8, label: "6" },
-            { value: 700 / 8, label: "7" },
-            { value: 100, label: "8" },
-          ]}
+          label={null}
+          defaultValue={[0, 100 / 8]}
+          step={100 / 8}
+          marks={MARKS}
+          onChangeEnd={setEndValue}
         />
       </div>
-      <div className="filtroTipos">
+      <div className="filtroTipos py-4">
         <h2>Tipos</h2>
-        
-        <Filter filterActive={false} clickHandler={toggleFilter} filterType={"normal"} icon={'XMarkIcon'}/>
+        <Grid className="hola" justify="center" columns={9}>
+          {PokemonTypes.map((type: string, i: number) => (
+            <Grid.Col span={3} md={1} key={i}>
+              <Filter filterType={type} filterLabel={type} icon={"XMarkIcon"} />
+            </Grid.Col>
+          ))}
+        </Grid>
       </div>
-      <div className="filtroGender">
-        <h2>Tipos</h2>
+      <div className="flex justify-between py-4">
+        <div className="filtroGender">
+          <h2>Género</h2>
+          <div className="flex gap-2">
+            <Filter filterLabel={"male"} icon={"XMarkIcon"} />
+            <Filter filterLabel={"female"} icon={"XMarkIcon"} />
+          </div>
+        </div>
+        <div className="filtroGender">
+          <h2>Legendaries</h2>
+          <Filter filterLabel={"legendaries"} icon={"XMarkIcon"} />
+        </div>
       </div>
-      <div className="filtroGender">
-        <h2>Tipos</h2>
-      </div>
+      <Button buttonType="outlined">Reset Filters</Button>
     </>
   );
 };
